5e0a47ec449ace5853e63437d2daa214464d5b14
starlike_daydream <2455443454@qq.com>
2024-01-31 14:44:26 +0800

    Site updated: 2024-01-31 14:44:18

--- js/matery.js
+++ js/matery.js
@@ -79,7 +79,7 @@ $(function () {
             $(this).addClass("img-shadow img-margin");
 
             //为文章内的图片添加no-referrer来隐藏referer（解决第三方图片外链不显示问题）
-            $(this).attr("referrerPolicy", "no-referrer");
+            $(this).attr('referrerPolicy','no-referrer');
 
             // 图片添加字幕
             let alt = $(this).attr('alt');
